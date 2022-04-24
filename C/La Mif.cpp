e263f637cbb90c5e6872be599d6d7200141a3370
Othmane Derrar <79415421+OthDr@users.noreply.github.com>
2022-04-24 01:07:33 +0100

    Update La Mif.cpp

--- C/La Mif.cpp
+++ C/La Mif.cpp
@@ -1,35 +1,5 @@
 #include <stdio.h>
 int main ()
-{int year;
-printf("\t\t\t\t\t\t\tBonjour!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-printf("Donnez votre annee de naissance:");
-scanf("%d",&year);
-printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-switch(year)
 {
-case (1969):		
-		printf("Vous etes: Derrar Mohamed\n\n\n\n\n\n");
-	break;
-case(1978):
-			printf("Vous etes: Bounessabia Faiza\n\n\n\n\n\n");
-	break;
-case (2000):			
-			printf("Vous etes: Derrar Othmane\n\n\n\n\n\n");
-	break;
-case (2004):			
-			printf("Vous etes: Derrar Imene\n\n\n\n\n\n");
-	break;	
-case (2007):			
-			printf("Vous etes: Derrar Nourhane\n\n\n\n\n\n");
-	break;	
-case (2010):			
-			printf("Vous etes: Derrar Assmahane_Djihane\n\n\n\n\n\n");
-	break;	
-case (2015):			
-			printf("Vous etes: Derrar Mohamed Kouider\n\n\n\n\n\n");
-	break;
-		
-}
-printf("Merci\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n Aurevoir");
 return 0;
 }
